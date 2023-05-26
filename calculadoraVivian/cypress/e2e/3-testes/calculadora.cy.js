eb2c3d70dc217cc0af1beb6ea63eb0a2baeacd56
yasjcarvalho <yasmin.carvalho@igdema.ufal.br>
2023-05-25 22:27:33 -0300

    teste exponenciação

--- calculadoraVivian/cypress/e2e/3-testes/calculadora.cy.js
+++ calculadoraVivian/cypress/e2e/3-testes/calculadora.cy.js
@@ -75,3 +75,44 @@ describe('testando função divisão', () => {
     cy.get('#resultado').should('have.text', 'Infinity')
   })
 })
+
+describe('testando função exponenciação', () => {
+  beforeEach(() => {
+    cy.visit('http://127.0.0.1:5500/calculadoraVivian/index.html')
+  })
+
+  it('subtração de 3 ^ 0', () => {
+    cy.get('#num1').type('3')
+    cy.get('#num2').type('0')
+    cy.get('[value="^"]').click()
+    cy.get('#resultado').should('have.text', '1')
+  })
+
+  it('subtração de 25 ^ 2', () => {
+    cy.get('#num1').type('25')
+    cy.get('#num2').type('2')
+    cy.get('[value="^"]').click()
+    cy.get('#resultado').should('have.text', '625')
+  })
+
+  it('subtração de 6^3', () => {
+    cy.get('#num1').type('6')
+    cy.get('#num2').type('3')
+    cy.get('[value="^"]').click()
+    cy.get('#resultado').should('have.text', '216')
+  })
+
+  it('subtração de 5 ^ 5', () => {
+    cy.get('#num1').type('5')
+    cy.get('#num2').type('5')
+    cy.get('[value="^"]').click()
+    cy.get('#resultado').should('have.text', '3125')
+  })
+
+  it('subtração de 2 ^ 2', () => {
+    cy.get('#num1').type('2')
+    cy.get('#num2').type('2')
+    cy.get('[value="^"]').click()
+    cy.get('#resultado').should('have.text', '4')
+  })
+})
